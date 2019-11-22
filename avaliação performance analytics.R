2004d4a50e166e4bff8517e54068f71a31e37a61
Di82Rquant <di82Rquant@gmail.com>
2019-11-22 11:34:52 -0300

    performance analytics

---
+++ avaliação performance analytics.R
@@ -0,0 +1,53 @@
+#
+
+
+# implementando indicadores
+#pacote especial para ativos
+require(quantmod)
+#pacote para indicadores
+require(TTR)
+#começo e fim do estudo
+startDate <- as.Date("2018-09-11")
+endDate   <- as.Date("2019-02-05")
+#ativos buscados na internet
+tickers <- c("^BVSP","PETR4.SA")
+
+#fonte dos dados
+getSymbols(tickers,src = "yahoo",from = startDate, to = endDate)
+#grafico de candles
+chartSeries(PETR4.SA,TA = NULL)
+#indicador
+addMACD()
+#criando estratégia
+macd <- MACD(PETR4.SA$PETR4.SA.Close,nFast = 12,nSlow =
+               26,nSig = 9,maType = SMA,percent = F)
+View(macd)
+#iniciando modelo
+tradesRulles <- lag(ifelse(macd$macd < macd$signal,-1,1))
+# -1 entrou vendido, ou 1 para compra
+#retorno com info do tradesRulles
+retornos <- ROC(PETR4.SA$PETR4.SA.Close)*tradesRulles
+#ROC = calcula os retornos
+#view retornos, tabela com valores de retorno
+View(retornos)
+#plotando resultados no grafico (data de plote)
+retornos <- retornos["2018-09-11/2019-02-05"]
+#carteira recebe de forma exp. os retornos
+#-1 para valores absolutos
+carteira <- exp(cumsum(retornos$PETR4.SA.Close))-1
+
+# o plot não funcionou
+plot(carteira,colors(T),col = "blue")
+
+==========================================================================
+  #avaliação de estratégia com performance analytics
+  # usei as variáveis da estratégia com indicador MACD
+
+  require(PerformanceAnalytics)
+#buscando os 10 melhores retornos
+table.Drawdowns(retornos,top = 10)
+#avaliando os riscos da estratégia
+table.DownsideRisk(retornos)
+#plotando evolução
+charts.PerformanceSummary(retornos)
+
